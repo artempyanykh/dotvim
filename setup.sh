5772ebf5c966ca937184f89cdb672fb742ee9842
Artem Pyanykh <a.pyanych@gmail.com>
2013-09-06 01:53:35 +0400

    Cleanups

--- setup.sh
+++ setup.sh
@@ -2,14 +2,13 @@ DOTFILESDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 echo "Applying VIM settings"
 cd $DOTFILESDIR
+[[ -d $HOME/.vim ]] && mv -f $HOME/.vim $HOME/.vim.old
+ln -sf "$DOTFILESDIR" "$HOME/.vim"
+ln -sb "$DOTFILESDIR/.vimrc" "$HOME/.vimrc"
 
 echo "Acquiring Vundle"
 git submodule init
 git submodule update
-
-[[ -d $HOME/.vim ]] && mv -f $HOME/.vim $HOME/.vim.old
-ln -sf "$DOTFILESDIR" $HOME/.vim
-ln -sb "$DOTFILESDIR/.vimrc" $HOME/.vimrc
 
 echo "Installing VIM plugins"
 vim +BundleInstall +qall
